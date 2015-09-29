6e950210d91f97acd593ce9337b236c17ef150de
mhv <Mikhail_Vroubel@r10-l2-s1-client74.telecom.by>
2015-09-29 20:23:49 +0300

    simple setup style

---
+++ Style.swift
@@ -0,0 +1,40 @@
+//
+//  Style.swift
+//  Style
+//
+//  Created by Mikhail Vroubel on 9/29/15.
+//  Copyright © 2015 Mikhail Vroubel. All rights reserved.
+//
+
+import UIKit
+import Utils
+
+public class Style : NSObject {
+    override public class func memoKey()-> String {
+        return "Style"
+    }
+    public let apply:(NSObject)->()
+    public init(apply:(NSObject)->()) {
+        self.apply = apply
+        super.init()
+    }
+    public convenience init(map:[String:String]) {
+        self.init {target in
+            _ = map.map { (key,val) in
+                target.setValue(val, forKey:key)
+            }
+        }
+    }
+}
+
+extension UIResponder {
+    public var styleName:String? {
+        set {
+            let styles = styleName?.componentsSeparatedByCharactersInSet(NSCharacterSet.whitespaceCharacterSet())
+            _ = styles?.map {
+                Style.memo($0)?.apply(self)
+            }
+        }
+        get {return nil}
+    }
+}
---
+++ Style/Style.h
@@ -0,0 +1,19 @@
+//
+//  Style.h
+//  Style
+//
+//  Created by Mikhail Vroubel on 9/29/15.
+//  Copyright © 2015 Mikhail Vroubel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Style.
+FOUNDATION_EXPORT double StyleVersionNumber;
+
+//! Project version string for Style.
+FOUNDATION_EXPORT const unsigned char StyleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Style/PublicHeader.h>
+
+
